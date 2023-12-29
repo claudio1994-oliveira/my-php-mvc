a5602acad2f9c522c67ad6fd5a48bd58a62f2524
Cláudio Oliveira <franciscoclaudiooliveira@gmail.com>
2023-12-29 11:00:35 -0300

    creating suites of tests + first tests

---
+++ tests/Feature/ExampleTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests\Feature;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
---
+++ tests/Feature/WelcomeTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Tests\Feature;
+
+use GuzzleHttp\Client;
+use PHPUnit\Framework\TestCase;
+
+class WelcomeTest extends TestCase
+{
+    public function test_that_the_page_welcome_can_render()
+    {
+        $url = 'http://localhost:8000/';
+
+        $client = new Client();
+        $response = $client->get($url);
+
+        $this->assertEquals(200, $response->getStatusCode());
+    }
+}
