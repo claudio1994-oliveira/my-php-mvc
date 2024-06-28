9323ceef3cc34bbf34911640f081748b43187d65
Claudio Oliveira <claudiooliveira@aridesa.com.br>
2024-06-28 17:29:27 -0300

    clean up controller

--- app/Http/Controller/WelcomeController.php
+++ app/Http/Controller/WelcomeController.php
@@ -3,32 +3,24 @@
 
 namespace App\Http\Controller;
 
-use App\Views\View;
 use App\Core\Container;
 use App\Core\Request;
-use App\Core\Response;
 
 class WelcomeController
 {
 
-    protected View $view;
     protected Request $request;
-    protected Response $response;
+
 
     public function __construct()
     {
-        $this->view = Container::getInstance()->get(View::class);
-        $this->request = Container::getInstance()->get(Request::class);
 
-        $this->response = new Response();
+        $this->request = Container::getInstance()->get(Request::class);
     }
 
     public function index()
     {
-        $this->response->withHeader('Content-Type', 'text/html');
 
-        $this->response->writeBody($this->view->render("welcome.welcome", ['title' => "Bem vindo!"]));
-
-        return $this->response->dispatch();
+        return view('welcome.welcome', ['title' => "Bem vindo!"]);
     }
 }
--- app/Support/Functions.php
+++ app/Support/Functions.php
@@ -1,1 +1,16 @@
 <?php
+
+use App\Core\Container;
+use App\Core\Response;
+use App\Views\View;
+
+function view(string $path, array $data = [])
+{
+    $view = Container::getInstance()->get(View::class);
+    $response = new Response();
+
+    $response->withHeader('Content-Type', 'text/html');
+    $response->writeBody($view->render($path, $data));
+
+    return $response->dispatch();
+}
