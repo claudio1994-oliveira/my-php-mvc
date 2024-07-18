4a5724bd68a79e2639aad4428c44942ddf6136f4
Cláudio Oliveira <franciscoclaudiooliveira@gmail.com>
2024-07-18 12:02:19 -0300

    alter query for users table using mysql driver

--- database/users.php
+++ database/users.php
@@ -10,6 +10,21 @@ include __DIR__ . '/../bootstrap/app.php';
 $pdo = Connector::getInstance();
 
 $pdo->exec('DROP TABLE IF EXISTS users;');
-$pdo->exec('CREATE TABLE users (id INTEGER PRIMARY KEY, name TEXT NOT NULL , username TEXT NOT NULL UNIQUE , email TEXT NOT NULL UNIQUE , password TEXT NOT NULL , created_at TIMESTAMP, updated_at TIMESTAMP);');
+
+if (env('DB_CONNECTION') == 'mysql') {
+    $pdo->exec('
+        CREATE TABLE users (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            name VARCHAR(255) NOT NULL,
+            username VARCHAR(255) NOT NULL UNIQUE,
+            email VARCHAR(255) NOT NULL UNIQUE,
+            password VARCHAR(255) NOT NULL,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+        );
+    ');
+} else {
+    $pdo->exec('CREATE TABLE users (id INTEGER PRIMARY KEY, name TEXT NOT NULL , username TEXT NOT NULL UNIQUE , email TEXT NOT NULL UNIQUE , password TEXT NOT NULL , created_at TIMESTAMP, updated_at TIMESTAMP);');
+}
 
 echo 'database created successfully ✌️😉' . PHP_EOL;
